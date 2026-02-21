9a6e1298d8ac794fb9ca51af808a53d36e6c4b6a
Khang <khangpropc345@gmail.com>
2026-02-21 17:22:01 +0700

    [fix]:router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/home/Home";
+import Layout from "./components/layout";
+import About from "./components/about/about";
+import Resume from "./components/resume";
+import Projects from "./components/projects";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route index element={<Layout />} />
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<Home />} />
-          <Route path="/resume" element={<Home />} />
-          <Route path="/projects" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/projects" element={<Projects />} />
         </Routes>
       </BrowserRouter>
     </>
